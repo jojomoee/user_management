ad2918cffe71dfe86e8b778b5d34776a4095fdd0
Johannes Moe <jojoomoe@protonmail.com>
2022-03-21 02:03:37 +0100

    _

--- package/core.py
+++ package/core.py
@@ -1,26 +1,15 @@
-from .dataClass import Person
-import JSONparser
+import dataclass
+import helpers  
 
 
-save_file_JSON = "persons.json"
+storage_json = "storage/storage.json"
+storage_xml = "storage/storage.xml"
 
-def newPerson():
-
-       inputPersNr = int(input("PersonNr: "))
-       inputFirstName = input("F Name: ")
-       inputLastName = input("L Name: ")
-       inputAge = int(input("Age: "))
-       inputSalary = float(input("Salary: "))
-
-       person = Person(inputPersNr,inputFirstName,inputLastName,inputAge,inputSalary)
-       person = JSONparser.toString(person)
-       personList = JSONparser.addPerson(person, save_file_JSON)
-       JSONparser.saveList(personList, save_file_JSON)
        
     
 
 def main():
-    newPerson()
+    dataclass.newPerson(storage_json)
 
 main()
 
--- package/dataclass.py
+++ package/dataclass.py
@@ -1,3 +1,5 @@
+import helpers
+
 class Person:
     def __init__(self, persId, firstName, lastName, age, salaray):
         self.persId = persId
@@ -8,6 +10,23 @@ class Person:
 
     def printPerson(self):
         print("{0:04d} {1:20s} {2:20s} {3} {4:8.2f}".format(self.persId, self.firstName, self.lastName, self.age, self.salaray))
+    
+def createPerson(): 
+    inputPersId = int(input("PersonId: "))
+    inputFirstName = input("F Name: ")
+    inputLastName = input("L Name: ")
+    inputAge = int(input("Age: "))
+    inputSalary = float(input("Salary: "))
+
+    person = Person(inputPersId, inputFirstName, inputLastName, inputAge, inputSalary)
+    return person
+
+def newPerson(saveFile):
+       person = createPerson()
+       personStr = helpers.toString(person)
+       personList = helpers.addPerson(personStr, saveFile)
+       helpers.saveList(personList, saveFile)
+
 
 #    def toJsonString(self):
 #       jsonString = json.dumps(self.__dict__)
